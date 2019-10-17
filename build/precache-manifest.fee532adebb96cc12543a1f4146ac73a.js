08f8d7d05762bdb9b14b85b4a4b727cf4e6af800
darts <darts@wifi_facstf-10-22-43-203.uncc.edu>
2019-10-17 14:15:17 -0400

    test build

---
+++ build/precache-manifest.fee532adebb96cc12543a1f4146ac73a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726ca6a14f9e6b32d1ae8fb2d74474b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ba960b0e0c3f5f47540",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a8b07fd30ca10371c068",
+    "url": "/static/js/2.8106aa7e.chunk.js"
+  },
+  {
+    "revision": "1ba960b0e0c3f5f47540",
+    "url": "/static/js/main.5ca55cb0.chunk.js"
+  },
+  {
+    "revision": "7ee92189f21cabce7960",
+    "url": "/static/js/runtime-main.90147e5b.js"
+  }
+]);
